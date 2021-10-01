bbaaf9d0d15063da353d6faf8b3cc676db518335
psatyam360 <psatyam360@gmail.com>
2021-10-01 19:41:36 +0530

    initial setup

---
+++ src/main/webapp/Java/com/crm/springmvc/entity/Customer.java
@@ -0,0 +1,46 @@
+package com.crm.springmvc.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
